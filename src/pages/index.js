f48e6169d67eb7221befca5d185961a57b595c62
Fedeya <elfedeminaya@gmail.com>
2020-06-09 05:54:51 -0300

    [Feat] used the home content

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Layout from "../components/layout";
 import HotelImage from '../components/hotelImage';
+import HomeContent from '../components/homeContent';
 
 const IndexPage = () => (
   <Layout>
     <HotelImage />
+    <HomeContent />
   </Layout>
 );
 
