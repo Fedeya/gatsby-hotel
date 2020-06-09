d74e0060d3cee1f403a31b353a7eb48e4192a9d2
Fedeya <elfedeminaya@gmail.com>
2020-06-09 06:49:36 -0300

    [Feat] used room preview

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,50 @@
 import React from "react";
+import styled from 'styled-components';
 
 import Layout from "../components/layout";
 import HotelImage from '../components/hotelImage';
 import HomeContent from '../components/homeContent';
+import RoomPreview from '../components/roomPreview'
 
-const IndexPage = () => (
-  <Layout>
-    <HotelImage />
-    <HomeContent />
-  </Layout>
-);
+import useRooms from '../hooks/use-rooms';
+
+const RoomsTitle = styled.h2`
+  text-align: center;
+  margin-top: 5rem;
+  font-size: 3rem;
+`;
+
+const RoomList = styled.ul`
+  max-width: 1200px;
+  width: 95%;
+  margin: 4rem auto 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem;
+  }
+`;
+
+const IndexPage = () => {
+
+  const rooms = useRooms();
+
+  return (
+    <Layout>
+      <HotelImage />
+      <HomeContent />
+      <RoomsTitle>Our Rooms</RoomsTitle>
+      <RoomList>
+        {
+          rooms.map(room => (
+            <RoomPreview key={room.id} room={room} />
+          ))
+        }
+      </RoomList>
+
+    </Layout>
+  );
+}
 
 export default IndexPage;
