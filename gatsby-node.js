72073c60fbbd1d57abf448ebd226d8444383fff2
Fedeya <elfedeminaya@gmail.com>
2020-06-09 07:06:32 -0300

    [Feat] created dynamic routing

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,30 @@
  */
 
 // You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result  = await graphql(`
+    query {
+      rooms: allDatoCmsRoom {
+        nodes {
+         slug
+        }
+      }
+    }
+  `);
+
+  if (result.error) {
+    reporter.panic('there where no results ', result.error);
+  }
+
+  const rooms = result.data.rooms.nodes;
+
+  rooms.forEach(room => {
+    actions.createPage({
+      path: room.slug,
+      component: require.resolve('./src/components/room.js'),
+      context: {
+        slug: room.slug
+      }
+    });
+  });
+}
