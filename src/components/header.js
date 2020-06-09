fffc12b5cbc4b8637e6e949ceb130e6ba0409f89
Fedeya <elfedeminaya@gmail.com>
2020-06-09 02:42:26 -0300

    First Commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,22 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react';
+import styled from 'styled-components';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const HeaderStyles = styled.header`
+  background-color: #333;
+  padding: 1rem;
+`;
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+const Heading  = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+const Header = () => (
+  <HeaderStyles>
+    <Heading>
+      <h1>Hotel Gatsby</h1>
+    </Heading>
+  </HeaderStyles>
+);
 
-export default Header
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,56 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from "react";
+import { Helmet } from 'react-helmet';
+import { createGlobalStyle } from 'styled-components';
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import Header from "./header";
 
-import Header from "./header"
-import "./layout.css"
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 16px;
+    font-size: 1.6rem;
+    line-height: 1.5;
+  }
+
+  h1, h2, h3 {
+    margin: 0;
+    line-height: 1.5;
+  }
+
+  h1, h2 {
+    font-family: 'Roboto', serif;
+  }
+
+  h3 {
+    font-family: 'PT Sans', sans-serif;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+`;
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Helmet>
+        <title>Gatsby Hotel</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap"/>
+      </Helmet>
+
+      <GlobalStyle />
+
+      <Header />
+      {children}
     </>
-  )
+  );
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
